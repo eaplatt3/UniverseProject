ef1e508f33ebeb755967a6da79125670d73dbab8
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-02-21 21:52:50 -0500

    Inital Commit
    Inital Commit

---
+++ Assets/Scripts/SpinMe.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinMe : MonoBehaviour
+    {
+
+    public int spd = 10;
+
+    public Transform trgt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.RotateAround(trgt.transform.position, trgt.transform.up, spd*Time.deltaTime);   
+    }
+}
